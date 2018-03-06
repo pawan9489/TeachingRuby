f7e6d59624c37f3c83dd96494fa39436ad0268bd
pawan9489 <pawankumar.gubbala@gmail.com>
2018-03-06 23:19:13 +0530

    sdaf

---
+++ DemoCode/Session1/1.putsVSprint.rb
@@ -0,0 +1,27 @@
+puts "Hello World"
+puts "This is second line"
+puts 3
+puts "Hi there, this is a 
+big line"
+
+
+# Line Breaks
+puts
+puts
+
+print "Hello World"
+print "This is second line"
+
+puts
+puts
+
+# Provides additional Details
+p "Hello World"
+p "This is second line"
+puts "2"
+p "2"
+p "Hi there, this is a 
+big line"
+
+
+# p vs puts Arrays Hashes Printing
