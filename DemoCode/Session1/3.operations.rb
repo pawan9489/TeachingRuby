5f2220fffe43da770201a2fb85b91ed9edcbc62f
pawan9489 <pawankumar.gubbala@gmail.com>
2018-03-07 07:50:39 +0530

    operations

--- DemoCode/Session1/3.operations.rb
+++ DemoCode/Session1/3.operations.rb
@@ -6,6 +6,11 @@ puts 14 <=> 14
 puts 14 <=> 12
 puts 14 <=> 16
 
+# Same data type and same value
+puts "5" == 5
+puts 5 == 5
+puts "5" == "5"
+puts 5 == 5.0
 
 # Assignment Operators
 puts ("assignment operator in Ruby")
@@ -22,3 +27,16 @@ puts ("assignment modulus")
 puts x %= 6
 puts ("assignment exponent")
 puts x **= 4
+
+# More readable methods
+
+puts 3.odd?
+puts 5.even?
+puts 3.between?(1,5)
+
+# Float Methods
+p 10.5.floor
+p 10.5.ceil
+p 3.14159.round(3)
+p -50.abs
+p 50.34.abs
