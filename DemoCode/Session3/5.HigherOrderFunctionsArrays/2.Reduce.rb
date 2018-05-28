e5c89fbd136a461f40162fca67a4a8ee67c6f5ab
pawan9489 <pawankumar.gubbala@gmail.com>
2018-05-28 07:58:23 +0530

    Session 4

--- DemoCode/Session3/5.HigherOrderFunctionsArrays/2.Reduce.rb
+++ DemoCode/Session3/5.HigherOrderFunctionsArrays/2.Reduce.rb
@@ -1,1 +1,17 @@
 # Reduce || Inject
+
+array = [1, 2, 3, 4] 
+p array.reduce(0, :+) 
+p array.reduce(0) { |sum, element| sum + element }
+
+p '---------------------------'
+p array.inject(0, :+) 
+
+# def add_3_multiply_4(n)
+#     (n + 3) * 4
+# end
+
+# Data
+fruits = ["apple", "orange", "pear", "strawberry", "banana", "mango"]
+
+p fruits.reduce("") {|concated_string, fruit| concated_string + " " + fruit }
--- DemoCode/Session3/5.HigherOrderFunctionsArrays/4.Find.rb
+++ DemoCode/Session3/5.HigherOrderFunctionsArrays/4.Find.rb
@@ -1,1 +1,8 @@
 # Find || Detect
+# finds the first element for which the given block returns true
+
+# Data
+fruits = ["apple", "orange", "pear", "strawberry", "banana", "mango"]
+
+p fruits.find {|fruit| fruit.length > 3}
+p fruits.detect {|fruit| fruit.length > 3}
